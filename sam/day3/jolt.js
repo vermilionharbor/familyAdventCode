e26cb0f1de0bf0baf8fdc4105c95c84bb267d212
Sam Wang <vermilionharbor@gmail.com>
2025-12-03 11:12:10 -0800

    Sam: day 3 part 2 complete.

--- sam/day3/jolt.js
+++ sam/day3/jolt.js
@@ -43,16 +43,67 @@ const getJolt = (maxTenMap, maxOneMap) => {
     return max10 * 10 + maxOnes;
 }
 
-let sum = 0;
-lines.forEach(l => {
-    // console.log(l);
-    const maxTenMap = createMap(l, l.length-2);
-    // console.log(maxTenMap);
-    const maxMap = createMap(l, l.length-1);
-    // console.log(maxMap);
-    const jolt = getJolt(maxTenMap, maxMap);
-    console.log(`${l} => jolt ${jolt}`);
-    sum += jolt;
-});
 
-console.log(`sum ${sum}`);
+const part1 = () => {
+    let sum = 0;
+    lines.forEach(l => {
+        // console.log(l);
+        const maxTenMap = createMap(l, l.length - 2);
+        // console.log(maxTenMap);
+        const maxMap = createMap(l, l.length - 1);
+        // console.log(maxMap);
+        const jolt = getJolt(maxTenMap, maxMap);
+        console.log(`${l} => jolt ${jolt}`);
+        sum += jolt;
+    });
+
+    console.log(`sum part 1 ${sum}`);
+}
+
+
+const getJolt2 = (maxMaps) => {
+    // there are 12 digits now and we will get 12 maps indexed 11..0
+    let digit = 11;
+    let offset = 0;
+    let acc = "";
+    // console.log(maxMaps);
+    while (digit >= 0) {
+        const digitMap = maxMaps[digit];
+        const val = digitMap[offset].val;
+        // add 1 to start the next cycle one place to the right
+        const nextIndex = digitMap[offset].index + 1;
+        acc += val.toString();
+        digit--;
+        offset = nextIndex;
+    }
+
+    return parseInt(acc, 10);
+}
+
+const part2 = () => {
+    let sum = 0;
+    lines.forEach(l => {
+        // there are 12 digits now
+        let maxMaps = {};
+        // offset 11 is the most significant digit
+        let offset = 11;
+        while (offset >= 0) {
+            // console.log(`computing map ${offset}`);
+            const digitMap = createMap(l, l.length - 1 - offset);
+            maxMaps[offset] = digitMap;
+            offset --;
+        }
+
+        const jolt = getJolt2(maxMaps)
+
+        console.log(`${l} => jolt ${jolt}`);
+        sum += jolt;
+    });
+
+    console.log(`sum part 2 ${sum}`);
+}
+
+console.log(`*** PART 1 ***`);
+part1();
+console.log(`\n\n*** PART 2 ***`);
+part2();

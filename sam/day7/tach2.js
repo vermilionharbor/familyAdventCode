225c3032bcc29c395b6cc338fd5a4165adf41db4
Sam Wang <vermilionharbor@gmail.com>
2025-12-10 15:22:17 -0800

    Sam: Day 7 part 2 done.

---
+++ sam/day7/tach2.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+
+const lines = fs.readFileSync('input.txt').toString().split("\n").filter(l => l !== "");
+
+
+const toIndex = (r, c) => `${r}_${c}`;
+const board = {}
+let startColumn;
+lines.forEach((l, r) => {
+    for (let c=0; c<l.length; c++) {
+        board[toIndex(r, c)] = l[c];
+        if (l[c] === 'S') {
+            startColumn = c;
+        }
+    }
+});
+
+const height = lines.length;
+const width = lines[0].length;
+
+const prettyPrint = (board, width, height) => {
+    for (let r=0; r<height; r++) {
+        let lineBuf = "";
+        for (let c=0; c<width; c++) {
+            lineBuf += board[toIndex(r, c)];
+        }
+        console.log(lineBuf)
+    }
+
+}
+
+prettyPrint(board, width, height);
+
+console.log(`startColumn ${startColumn}`);
+
+let beamMap = {};
+beamMap[startColumn] = 1;
+let numSplits = 0;
+for (let row=1; row<height-1; row++) {
+    let nextMap = {};
+    let nextRow = row+1;
+
+    // console.log(`processing row ${row} width ${width}`);
+    for (let b of Object.keys(beamMap)) {
+        // there's a beam at each b
+        const col = parseInt(b, 10);
+        const val = board[toIndex(nextRow, col)];
+        if (val === ".") {
+            // beam continues
+            if (nextMap[col] === undefined) {
+                nextMap[col] = beamMap[col];
+            } else {
+                nextMap[col] = nextMap[col] + beamMap[col];
+            }
+        } else if (val === "^") {
+            numSplits++;
+            console.log(`split at ${row+1}, ${col}`)
+            // hit a splitter, split the values
+            if (col-1 >= 0) {
+                if (nextMap[col-1] === undefined) {
+                    nextMap[col-1] = beamMap[col];
+                }  else {
+                    nextMap[col-1] = nextMap[col-1] + beamMap[col];
+                }
+            }
+            if (col+1 < width) {
+                if (nextMap[col +1] === undefined) {
+                    nextMap[col+1] = beamMap[col];
+                } else {
+                    nextMap[col+1] = nextMap[col+1] + beamMap[col];
+                }
+            }
+        }
+    }
+    console.log(`row ${row} ${JSON.stringify(nextMap)}`);
+    beamMap = nextMap;
+}
+
+console.log(`Complete, numSplits ${numSplits}`);
+console.log(beamMap);
+let numTimeLines = 0;
+for (let [k, v] of Object.entries(beamMap)) {
+    numTimeLines += v;
+}
+
+console.log(`num time lines ${numTimeLines}`);
